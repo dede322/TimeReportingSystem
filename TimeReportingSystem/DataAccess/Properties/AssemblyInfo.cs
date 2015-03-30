87c734a5d47e205cbe3c8a165f7692b7313fcc6e
dede322 <dede3223232@mail.ru>
2015-03-30 22:40:18 +0400

    Создание

--- TimeReportingSystem/DataAccess/Properties/AssemblyInfo.cs
+++ TimeReportingSystem/DataAccess/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Core")]
+[assembly: AssemblyTitle("DataAccess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Core")]
+[assembly: AssemblyProduct("DataAccess")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("378dffbf-ce21-48aa-860c-ec415455d96b")]
+[assembly: Guid("a03d72a1-232f-4feb-90d3-8deec00c9567")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
