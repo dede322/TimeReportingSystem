1735b427e5a20527690fed58b265fcfce0c83e11
dede322 <dede3223232@mail.ru>
2015-03-30 23:05:52 +0400

    Джсон репозиторий

---
+++ TimeReportingSystem/DataAccess/JsonRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class JsonRepository<T> :IRepository<T>
+    {
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modify(T element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
